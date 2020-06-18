5adf680eba7e5b53c07f050f6726096079df50be
Keroiw <blazej.wiorek@gmail.com>
2020-06-18 16:33:17 +0200

    Pesistence of partial results

--- ComplexData/eurostat_data_creation.R
+++ ComplexData/eurostat_data_creation.R
@@ -5,13 +5,6 @@ library(stringr)
 library(purrr)
 library(ggplot2)
 library(lubridate)
-#library(mice)
-#library(bigstep)
-#library(doBy)
-#library(randomForest)
-#library(lme4)
-#library(sjPlot)
-#library(corrplot)
 
 
 trim_geo <- function(df) df %>%
@@ -34,129 +27,172 @@ reduce_columns <- function(df) {
     select(c(geo, year_fct, values))
 }
 
-population <- get_eurostat("demo_pjan", type="label") %>% postprocess()
-employment <- get_stat("t2020_10")
-debt <- get_stat("teina225")
-poverty <- get_stat("ilc_peps01")
-ind_growth_rates <- get_eurostat("ei_isir_m", type="label", select_time = "M") %>% postprocess() 
-immigration <- get_stat("migr_imm8")
-emmigration <- get_stat("migr_emi2")
-
 base_cols <- c("country", "year_fct")
 
 ### Population
 
-population.clean <- population %>% 
-  filter(unit=="Number", age=="Total", sex=="Total") %>%
-  reduce_columns()
-colnames(population.clean) <- c(base_cols, "population")
+get_population <- function() {
+  population <- get_eurostat("demo_pjan", type="label") %>% postprocess()
+  population.clean <- population %>% 
+    filter(unit=="Number", age=="Total", sex=="Total") %>%
+    reduce_columns()
+  colnames(population.clean) <- c(base_cols, "population")
+  population.clean
+}
 
 ### Employment
 
-employment.clean <- employment %>%
-  filter(sex == "Total") %>% 
-  reduce_columns()
-colnames(employment.clean) <- c(base_cols, "emp_pct")
+get_employment <- function() {
+  employment <- get_stat("t2020_10")
+  employment.clean <- employment %>%
+    filter(sex == "Total") %>% 
+    reduce_columns()
+  colnames(employment.clean) <- c(base_cols, "emp_pct")
+  employment.clean
+}
 
 ### Debt
 
-debt.clean <- debt %>% 
-  filter(grepl("^Percentage", unit)) %>%
-  reduce_columns()
-colnames(debt.clean) <- c(base_cols, "debt_pct_gdp")
+get_debt <- function() {
+  debt <- get_stat("teina225")
+  debt.clean <- debt %>% 
+    filter(grepl("^Percentage", unit)) %>%
+    reduce_columns()
+  colnames(debt.clean) <- c(base_cols, "debt_pct_gdp")
+  debt.clean
+}
 
 ### Poverty
 
-poverty.clean <- poverty %>% 
-  filter(sex == "Total" & age == "Total", unit == "Percentage") %>%
-  reduce_columns()
-colnames(poverty.clean) <- c(base_cols, "risk_poverty_pct")
+get_poverty <- function() {
+  poverty <- get_stat("ilc_peps01")
+  poverty.clean <- poverty %>% 
+    filter(sex == "Total" & age == "Total", unit == "Percentage") %>%
+    reduce_columns()
+  colnames(poverty.clean) <- c(base_cols, "risk_poverty_pct")
+  poverty.clean
+}
 
-### HICP
-
-hicp <- read.csv('/home/wiorek/Desktop/UWR-Lectures/ComplexData/hicp_full_europe.csv', skip=2)
-hicp.clean <- hicp %>% 
-  filter(grepl("20", X)) %>% 
-  separate(col=X, into=c("year", "month"), sep=4) %>% 
-  filter(year>=2005 & year <= 2018) %>% 
-  pivot_longer(-c("year", "month"), names_to="country", values_to="hicp") %>%
-  mutate(hicp = as.numeric(levels(hicp))[hicp],
-         country = recode(country, `Czech.Republic`="czechia", `United.Kingdom`="united kingdom"),
-         country = factor(toupper(country)),
-         year_fct = ordered(as.numeric(year))) %>%
-  select(-year) %>%
-  group_by(year_fct, country) %>%
-  summarise(hicp=mean(hicp), .groups="drop")
 
 ### Industry growth rates
 
-ind_growth_rates.clean <- ind_growth_rates %>% 
-  filter(grepl("Industry", nace_r2)) %>%
-  filter(grepl("Growth rate on previous", unit)) %>%
-  mutate(month = as.numeric(format(time,'%m')), 
-         year = as.numeric(format(time,'%Y'))) %>%
-  select(geo, values, month, year) %>%
-  group_by(geo, year) %>%
-  summarise(an_ind_rate=sum(values), .groups="drop") %>%
-  filter(year >= 2005 & year <= 2018) %>%
-  mutate(year_fct = as.ordered(year)) %>%
-  rename(country=geo) %>%
-  select(-year)
+get_ind_rates <- function() {
+  ind_growth_rates <- get_eurostat("ei_isir_m", type="label", select_time = "M") %>% 
+    postprocess()
+  ind_growth_rates.clean <- ind_growth_rates %>% 
+    filter(grepl("Industry", nace_r2)) %>%
+    filter(grepl("Growth rate on previous", unit)) %>%
+    mutate(month = as.numeric(format(time,'%m')), 
+           year = as.numeric(format(time,'%Y'))) %>%
+    select(geo, values, month, year) %>%
+    group_by(geo, year) %>%
+    summarise(an_ind_rate=sum(values), .groups="drop") %>%
+    filter(year >= 2005 & year <= 2018) %>%
+    mutate(year_fct = as.ordered(year)) %>%
+    rename(country=geo) %>%
+    select(-year)
+  ind_growth_rates.clean
+}
 
 ### Immigration 
 
-immigration.clean <- immigration %>% 
-  filter(unit=="Number", sex=="Total", age=="Total", agedef=="Age in completed years") %>%
-  reduce_columns()
-colnames(immigration.clean) <- c(base_cols, "immigration")
+get_immigration <- function() {
+  immigration <- get_stat("migr_imm8")
+  immigration.clean <- immigration %>% 
+    filter(unit=="Number", sex=="Total", age=="Total", agedef=="Age in completed years") %>%
+    reduce_columns()
+  colnames(immigration.clean) <- c(base_cols, "immigration")
+  immigration.clean
+}
 
 ### Emmigration
 
-emmigration.clean <- emmigration %>% 
-  filter(unit=="Number", sex=="Total", age=="Total", agedef=="Age in completed years") %>%
-  reduce_columns()
-colnames(emmigration.clean) <- c(base_cols, "emmigration") 
+get_emmigration <- function() {
+  emmigration <- get_stat("migr_emi2")
+  emmigration.clean <- emmigration %>% 
+    filter(unit=="Number", sex=="Total", age=="Total", agedef=="Age in completed years") %>%
+    reduce_columns()
+  colnames(emmigration.clean) <- c(base_cols, "emmigration") 
+  emmigration.clean
+}
 
+### HICP
+
+get_hicp <- function() {
+  hicp <- read.csv('/home/wiorek/Desktop/UWR-Lectures/ComplexData/hicp_full_europe.csv', skip=2)
+  hicp.clean <- hicp %>% 
+    filter(grepl("20", X)) %>% 
+    separate(col=X, into=c("year", "month"), sep=4) %>% 
+    filter(year>=2005 & year <= 2018) %>% 
+    pivot_longer(-c("year", "month"), names_to="country", values_to="hicp") %>%
+    mutate(hicp = as.numeric(levels(hicp))[hicp],
+           country = recode(country, `Czech.Republic`="czechia", `United.Kingdom`="united kingdom"),
+           country = factor(toupper(country)),
+           year_fct = ordered(as.numeric(year))) %>%
+    select(-year) %>%
+    group_by(year_fct, country) %>%
+    summarise(hicp=mean(hicp), .groups="drop")
+  hicp.clean
+}
 
 ### Gdp-Per-Cap
 
-gdp_per_cap <- read.csv('/home/wiorek/Desktop/UWR-Lectures/ComplexData/gdp_per_cap.csv', skip=4)
-gdp_per_cap.clean <- gdp_per_cap %>%
-  mutate(country = recode(Country.Name, `Czech Republic`="Czechia", `Slovak Republic`="Slovakia")) %>%
-  filter(country %in% eu_countries$name) %>%
-  select(country, everything(), -c("Country.Code", "Indicator.Name", "Indicator.Code", "Country.Name")) %>%
-  pivot_longer(-c("country"), names_to="year", values_to="gdp_per_cap") %>%
-  mutate(year = as.numeric(str_remove(year, 'X'))) %>%
-  filter(year >= 2005 & year <= 2018) %>%
-  mutate(country = as.factor(toupper(country)), 
-         year_fct = as.ordered(year)) %>%
-  select(-c("year"))
+get_gdp <- function() {
+  gdp_per_cap <- read.csv('/home/wiorek/Desktop/UWR-Lectures/ComplexData/gdp_per_cap.csv', skip=4)
+  gdp_per_cap.clean <- gdp_per_cap %>%
+    mutate(country = recode(Country.Name, `Czech Republic`="Czechia", `Slovak Republic`="Slovakia")) %>%
+    filter(country %in% eu_countries$name) %>%
+    select(country, everything(), -c("Country.Code", "Indicator.Name", "Indicator.Code", "Country.Name")) %>%
+    pivot_longer(-c("country"), names_to="year", values_to="gdp_per_cap") %>%
+    mutate(year = as.numeric(str_remove(year, 'X'))) %>%
+    filter(year >= 2005 & year <= 2018) %>%
+    mutate(country = as.factor(toupper(country)), 
+           year_fct = as.ordered(year)) %>%
+    select(-c("year"))
+}
 
 ## Merge datasets
 
-years <- seq(from=2005, to=2018, by=1)
-measurment_years <- rep(years, each=length(eu_countries$name))
-countries <- rep(unique(population.clean$country), each=length(years))
-dataset <- data.frame(year_fct = as.ordered(years),
-                      country = toupper(countries))
+merge_tables <- function() {
+  population.clean <- get_population()
+  employment.clean <- get_employment()
+  debt.clean <- get_debt()
+  hicp.clean <- get_hicp()
+  poverty.clean <- get_poverty()
+  gdp_per_cap.clean <- get_gdp()
+  ind_growth_rates.clean <- get_ind_rates()
+  immigration.clean <- get_immigration()
+  emmigration.clean <- get_emmigration()
+  
+  years <- seq(from=2005, to=2018, by=1)
+  measurment_years <- rep(years, each=length(eu_countries$name))
+  countries <- rep(unique(population.clean$country), each=length(years))
+  dataset <- data.frame(year_fct = as.ordered(years),
+                        country = toupper(countries))
+  
+  dfs <- list(dataset, 
+              population.clean, 
+              employment.clean, 
+              debt.clean, 
+              hicp.clean, 
+              poverty.clean, 
+              gdp_per_cap.clean, 
+              ind_growth_rates.clean, 
+              immigration.clean,
+              emmigration.clean)
+  
+  dataset <- reduce(dfs, function(df_l, df_r) merge(df_l, df_r, all.x=T)) 
+  dataset
+}
 
-dfs <- list(dataset, 
-            population.clean, 
-            employment.clean, 
-            debt.clean, 
-            hicp.clean, 
-            poverty.clean, 
-            gdp_per_cap.clean, 
-            ind_growth_rates.clean, 
-            immigration.clean,
-            emmigration.clean)
-
-dataset <- reduce(dfs, function(df_l, df_r) merge(df_l, df_r, all.x=T)) 
+dataset.merged <- merge_tables()
+write.table(dataset.merged, "/home/wiorek/Desktop/UWR-Lectures/ComplexData/Eurostat_Files/dataset_merged.csv", row.names=T)
 
 ## Imputation
 
-# dataset.full %>% filter(is.na(an_ind_rate)) # - the only missing value for an_ind_rate
-dataset.filled <- dataset %>% mutate(an_ind_rate = replace_na(an_ind_rate, 0))
+dataset.merged <- read.table("/home/wiorek/Desktop/UWR-Lectures/ComplexData/Eurostat_Files/dataset_merged.csv", row.names=1)
+dataset.merged <- dataset.merged %>% mutate(year_fct = as.ordered(year_fct))
+dataset.filled <- dataset.merged %>% mutate(an_ind_rate = replace_na(an_ind_rate, 0))
 
 get_closest <- function(dataset, col_name, country_name, n=3) {
   X <- dataset %>% filter(country != country_name) %>% select(c("country",  col_name, "year_fct"))
@@ -170,7 +206,7 @@ get_closest <- function(dataset, col_name, country_name, n=3) {
   euclid_diff <- apply(euclid_diff, 2, function(x) sqrt(sum(x^2)))
   countries <- colnames(X_wide)
   countries <- countries[2:length(countries)]
-  list(dist=euclid_diff[which.minn(euclid_diff, n)], na_years=na_years)
+  list(dist=euclid_diff[doBy::which.minn(euclid_diff, n)], na_years=na_years)
 }
 
 fill_missing <- function(data, country_name, closest_observ, na_years, col_name) {
@@ -203,13 +239,7 @@ dataset.filled$imputed_vals %>% ggplot(aes(x=year_fct, y=risk_poverty_pct, color
 dataset.filled <- impute(dataset.filled$imputed, "risk_poverty_pct", "ROMANIA", n=2)
 dataset.filled$imputed_vals %>% ggplot(aes(x=year_fct, y=risk_poverty_pct, color=imputed)) + geom_point()
 
-# For Bulgaria an outlier was generated hence I've checked the reports and 57% sound reasonable
-#dataset.filled <- impute(dataset.filled$imputed, "risk_poverty_pct", "BULGARIA", n=2)
-#dataset.filled$imputed_vals %>% ggplot(aes(x=year_fct, y=risk_poverty_pct, color=imputed)) + geom_point()
-
 dataset.filled <- dataset.filled$imputed %>% mutate(risk_poverty_pct = replace_na(risk_poverty_pct, 57))
-
-
 dataset.filled %>% filter(country=="BULGARIA") %>% ggplot(aes(x=year_fct, y=immigration)) + geom_point()
 
 ## Immigration
@@ -359,17 +389,33 @@ dataset.normalized$emmigration <- (dataset.full$emmigration / dataset.full$popul
 dataset.normalized$population <- dataset.full$population / 10000000
 dataset.normalized$gdp_per_cap <- dataset.full$gdp_per_cap/ 10000
 
-## Imputing with RF
-#dataset.to_rf_impute <- dataset.full %>% select(-an_ind_rate, -state, -country, -risk_poverty_pct, -year) 
-#dataset.rf_imputed <- rfImpute(hdi ~ ., dataset.to_rf_impute)
-#dataset.rf_imputed$country <- dataset.full$country
-#dataset.rf_imputed$year <- dataset.full$year
+write.table(dataset.merged, "/home/wiorek/Desktop/UWR-Lectures/ComplexData/Eurostat_Files/dataset_normalized.csv", row.names=T)
 
-### check results of imputation
+#### Longitudinal
 
-#bulgaria_missing <- dataset.full %>% filter(country=="BULGARIA") %>% select(year, immigration)
-#bulgaria_imputed <- dataset.rf_imputed %>% filter(country=="BULGARIA") %>% select(year, immigration)
-#missing_idx <- which(is.na(bulgaria_missing[,2]))
-#bulgaria_imputed$imputed = "No"
-#bulgaria_imputed[missing_idx, "imputed"] = "Yes"
-#bulgaria_imputed %>% ggplot(aes(x=year, y=immigration, color=imputed)) + geom_point()
+dataset.normalized <- read.table("/home/wiorek/Desktop/UWR-Lectures/ComplexData/Eurostat_Files/dataset_normalized.csv", row.names=1)
+
+to_diff <- setdiff(colnames(dataset.normalized), c("country", "year", "state", "immigration", "emmigration"))
+to_sum <- c("an_ind_rate")
+make_longit <- function(country_data) {
+  baseline <- country_data[country_data$year == 2005,]
+  years <- sort(unique(country_data$year))
+  longit <- country_data
+  i = 1
+  for (year in years[2:length(years)]) {
+    df_row <-  country_data[country_data$year == year, ]
+    longit[country_data$year == year, to_diff] <-  df_row[,to_diff] - baseline[,to_diff]
+    longit[country_data$year == year, to_sum] <- tail(cumsum(country_data[1:i, to_sum]), n=1)
+    i = i + 1
+  }
+  longit[-1, ]
+}
+
+country_data <- list()
+for (country_name in unique(dataset.normalized$country)) {
+  country_data[[country_name]] <- dataset.normalized %>% filter(country==country_name)
+}
+
+dataset.longit <- dplyr::bind_rows(lapply(country_data, make_longit))
+
+write.table(dataset.longit, "/home/wiorek/Desktop/UWR-Lectures/ComplexData/Eurostat_Files/dataset_longit.csv", row.names=T)
